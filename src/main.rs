a51cb53aeb001f17fbe732f265d12a50a2cca248
janosimas <janosimas@gmail.com>
2020-04-18 08:13:12 +0200

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{executor, Application, Column, Command, Element, Settings, Text};
+
+pub fn main() {
+    Game::run(Settings::default())
+}
+
+struct Game {
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+        }
+    }
+}
+
+impl Application for Game {
+    type Executor = executor::Null;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (Self::new(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Language game")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Column::new()
+            .spacing(10)
+            .push(
+                Text::new("test")
+            )
+            .into()
+    }
 }
