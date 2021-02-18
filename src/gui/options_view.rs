f72e595d3b6b085930c0f900274001ab2d7cb044
janosimas <janosimas@gmail.com>
2021-02-18 15:04:36 +0100

    Add target score option field

--- src/gui/options_view.rs
+++ src/gui/options_view.rs
@@ -1,18 +1,41 @@
-use iced::{Command, Element, Text};
+use general::Options;
+use iced::{text_input, Column, Command, Element, Text, TextInput};
 
 use crate::general;
 
-pub struct OptionsView {}
+pub struct OptionsView {
+    target_score_state: text_input::State,
+    temp_target_score: String,
+}
 
 impl OptionsView {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            target_score_state: text_input::State::default(),
+            temp_target_score: String::default(),
+        }
     }
 
-    pub fn update(&mut self, _message: general::Message) -> Command<general::Message> {
-        todo!()
+    pub fn update(&mut self, message: general::Message) -> Command<general::Message> {
+        if let general::Message::Options(Options::AlterTargetScore(new_target_score)) = message {
+            if new_target_score.parse::<u32>().is_ok() || new_target_score.is_empty() {
+                self.temp_target_score = new_target_score;
+            }
+        }
+
+        Command::none()
     }
     pub fn view(&mut self) -> Element<general::Message> {
-        Text::new("Options::view").into()
+        Column::new()
+            .push(Text::new("Options::view"))
+            .push(TextInput::new(
+                &mut self.target_score_state,
+                "Target score value",
+                &self.temp_target_score,
+                |new_target_score| {
+                    general::Message::Options(Options::AlterTargetScore(new_target_score))
+                },
+            ))
+            .into()
     }
 }
